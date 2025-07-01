77a838f8123006af04019acd9fdcd6711a5c867e
Milan Lamichhane <milan.np@gmail.com>
2025-07-01 14:55:02 +0545

    Added functions for various tree traversal methods.

--- BinaryTree.py
+++ BinaryTree.py
@@ -31,20 +31,53 @@ class BinaryTree:
     
     def get_data(self):
         return self.data
+    
+def inorder(root_node):
+    current = root_node
+    if current is None:
+        return
+    inorder(current.left_child)
+    print(current.data)
+    inorder(current.right_child)
 
+def preorder(root_node):
+    current = root_node
+    if current is None:
+        return
+    print(current.data)
+    preorder(current.left_child)
+    preorder(current.right_child)
+
+def postorder( root_node):
+    current = root_node
+    if current is None:
+        return
+    postorder(current.left_child)
+    postorder(current.right_child)
+    print(current.data)
 
 if __name__ == "__main__":
     r = BinaryTree('a')
-    print(r.get_data()) 
-    print(r.get_left_child())
+    # print(r.get_data()) 
+    # print(r.get_left_child())
     r.insert_left('b')
-    print(r.get_left_child())
-    print(r.get_left_child().get_data())
+    # print(r.get_left_child())
+    # print(r.get_left_child().get_data())
     r.insert_right('c') 
-    print(r.get_right_child()) 
-    print(r.get_right_child().get_data()) 
-    r.get_right_child().set_data('hello') 
-    print(r.get_right_child().get_data())
+    # print(r.get_right_child()) 
+    # print(r.get_right_child().get_data()) 
+    # r.get_right_child().set_data('hello') 
+    # print(r.get_right_child().get_data())
 
+    print("In-order Traversal")
+    inorder(r)
+    print("-------------")
+    print("Pre-order Traversal")
+    preorder(r)
+    print("-------------")
+    print("Post-order Traversal")
+    postorder(r)
+    print("-------------")
 
+    
 
