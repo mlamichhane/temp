b74f372b31ae77d9ef3b4bbdec280fdb4e32de0a
Milan Lamichhane <milan.np@gmail.com>
2025-12-01 15:11:54 +0545

    Updated program

--- CreditCardApp.py
+++ CreditCardApp.py
@@ -2,9 +2,20 @@ from CreditCard import CreditCard
 
 if __name__ == '__main__':
     cc = CreditCard('John Doe', 'Bank of Python', '1234 5678 9012 3456', 5000)
-    
+
     print(f'Customer: {cc.get_customer()}')
     print(f'Bank: {cc.get_bank()}')
     print(f'Account: {cc.get_account()}')
     print(f'Limit: {cc.get_limit()}')
     print(f'Initial Balance: {cc.get_balance()}')
+
+    cc.make_payment(8000)
+    print(f'Balance: {cc.get_balance()}')
+
+    if cc.charge(3000):
+        print('Charge approved.')
+    else:
+        print('Charge denied.')
+    
+    print(f'Balance: {cc.get_balance()}')
+
