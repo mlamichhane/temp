a2f4cbe953c33807399711fe36ee3ea94ed7c22e
Milan Lamichhane <milan.np@gmail.com>
2025-12-02 18:59:48 +0545

    Created multiple instances of CreditCard class.

--- CreditCardApp.py
+++ CreditCardApp.py
@@ -1,21 +1,23 @@
 from CreditCard import CreditCard
 
 if __name__ == '__main__':
-    cc = CreditCard('John Doe', 'Bank of Python', '1234 5678 9012 3456', 5000)
 
-    print(f'Customer: {cc.get_customer()}')
-    print(f'Bank: {cc.get_bank()}')
-    print(f'Account: {cc.get_account()}')
-    print(f'Limit: {cc.get_limit()}')
-    print(f'Initial Balance: {cc.get_balance()}')
+    visaCard = CreditCard('Ram Bahadur', 'NIMB', '1234 5678 9012 3456', 5000)
+    masterCard = CreditCard('Hari Gopal', 'Nabil Bank', '1010 2020 3356 9657', 15000)
 
-    cc.make_payment(8000)
-    print(f'Balance: {cc.get_balance()}')
+    print(f'Customer: {visaCard.get_customer()}')
+    print(f'Bank: {visaCard.get_bank()}')
+    print(f'Account: {visaCard.get_account()}')
+    print(f'Limit: {visaCard.get_limit()}')
+    print(f'Initial Balance: {visaCard.get_balance()}')
 
-    if cc.charge(3000):
+    visaCard.make_payment(8000)
+    print(f'Balance: {visaCard.get_balance()}')
+
+    if visaCard.charge(3000):
         print('Charge approved.')
     else:
         print('Charge denied.')
     
-    print(f'Balance: {cc.get_balance()}')
+    print(f'Balance: {visaCard.get_balance()}')
 
