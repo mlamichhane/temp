872c1e9eff6209cc2349f8132fee6adf0d551414
Milan Lamichhane <milan.np@gmail.com>
2026-02-06 15:39:40 +0545

    Added a simpler version of the Linked List implementation

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -1,65 +1,81 @@
-class DListNode :
+class Node:
     def __init__(self, data):
         self.data = data
-        self.next = None
         self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        """Add a node to the end of the list."""
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        last = self.head
+        while last.next:
+            last = last.next
+        
+        last.next = new_node
+        new_node.prev = last
+
+    def prepend(self, data):
+        """Add a node to the beginning of the list."""
+        new_node = Node(data)
+        new_node.next = self.head
+        if self.head:
+            self.head.prev = new_node
+        self.head = new_node
+
+    def delete(self, data):
+        """Delete the first occurrence of a node with given data."""
+        current = self.head
+        while current:
+            if current.data == data:
+                # Node is the head
+                if current.prev is None:
+                    self.head = current.next
+                    if self.head:
+                        self.head.prev = None
+                else:
+                    current.prev.next = current.next
+                    if current.next:
+                        current.next.prev = current.prev
+                return
+            current = current.next
+
+    def display_forward(self):
+        """Display the list in forward direction."""
+        current = self.head
+        while current:
+            print(current.data, end=' ')
+            current = current.next
+        print()
+
+    def display_backward(self):
+        """Display the list in reverse direction."""
+        current = self.head
+        if not current:
+            return
+        # Go to the last node
+        while current.next:
+            current = current.next
+        # Traverse backward
+        while current:
+            print(current.data, end=' ')
+            current = current.prev
+        print()
 
 
-def revTraversal(tail):
-    curNode = tail
-    while curNode is not None:
-        print(curNode.data)
-        curNode = curNode.prev
-
-
-# Given the head, tail, and probe references, probe the list for a target.
-def probList(head, tail, probe, target):
-    # Make sure the list is not empty.
-    if head is None:
-        return False
-    # If probe is null, initialize it to the first node.
-    elif probe is None:
-        probe = head
-
-    # If the target comes before the probe node, we traverse backward;
-    # otherwise traverse forward.
-    if target < probe.data:
-        while probe is not None and target <= probe.data:
-            if target == probe.data:
-                return True
-            else:
-                probe = probe.prev
-    else:
-        while probe is not None and target >= probe.data:
-            if target == probe.data:
-                return True
-            else:
-                probe = probe.next
-
-    # If the target is not found in the list, return False.
-    return False
-
-# Given a head and tail reference and a new value, add the new value to a
-# sorted doubly linked list.
-def addNode(head, tail, value):
-    newnode = DListNode(value)
-    if head is None: # empty list
-        head = newnode
-        tail = head
-    elif value < head.data: # insert before head
-        newnode.next = head
-        head.prev = newnode
-        head = newnode
-    elif value > tail.data: # insert after tail
-        newnode.prev = tail
-        tail.next = newnode
-        tail = newnode
-    else: # insert in the middle
-        node = head
-        while node is not None and node.data < value:
-            node = node.next
-
-        newnode.next = node
-        newnode.prev = node.prev
-        node.prev.next = newnode
-        node.prev = newnode
+## Example Usages
+dll = DoublyLinkedList()
+dll.append(10)
+dll.append(20)
+dll.prepend(5)
+dll.display_forward()   # Output: 5 10 20
+dll.display_backward()  # Output: 20 10 5
+dll.delete(10)
+dll.display_forward()   # Output: 5 20
